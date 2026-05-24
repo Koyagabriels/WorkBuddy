4a4dc52469671927be43453f30cad1bcbea3a16e
Koyagabriels <107101679+Koyagabriels@users.noreply.github.com>
2026-05-24 17:10:24 +0100

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { DataChat } from './components/chat/DataChat';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>WorkBuddy</h1>
+      <DataChat />
+    </div>
+  );
+}
+
+export default App;
